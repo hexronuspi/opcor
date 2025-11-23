e4fdf00abedf1bd494f3e55c988d9cb8f9c6fdc3
Aditya Raj <115471257+hexronuspi@users.noreply.github.com>
2025-11-23 16:43:08 +0530

    Delete Desktop directory

--- Desktop/cosly/src/app/dashboard/question_bank/[title]/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function QuestionDetailLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
--- Desktop/cosly/src/app/dashboard/question_bank/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function QuestionBankLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
